e5c5911c2f7fbc1d34cd70861bf9976c65c00fa4
Miroslav Schlossberg <mschlos@gmail.com>
2019-07-11 21:24:12 +0200

    alaveteli 0.28

--- lib/controller_patches.rb
+++ lib/controller_patches.rb
@@ -1,3 +1,4 @@
+# -*- encoding : utf-8 -*-
 # Add a callback - to be executed before each request in development,
 # and at startup in production - to patch existing app classes.
 # Doing so in init/environment.rb wouldn't work in development, since
@@ -5,14 +6,15 @@
 # See http://stackoverflow.com/questions/7072758/plugin-not-reloading-in-development-mode
 #
 Rails.configuration.to_prepare do
-    # Example adding an instance variable to the frontpage controller
-    GeneralController.class_eval do
-        def mycontroller
-            @say_something = "Greetings friend"
-        end
-    end
-    HelpController.class_eval do
-        def help_out
-        end
-    end
+  # Example adding an instance variable to the frontpage controller
+  # GeneralController.class_eval do
+  #   def mycontroller
+  #     @say_something = "Greetings friend"
+  #   end
+  # end
+  # Example adding a new action to an existing controller
+  # HelpController.class_eval do
+  #   def help_out
+  #   end
+  # end
 end
