f4fb4fcb8bc0a67612147c11e4e0e99793b61dff
Schlossy <schlos@users.noreply.github.com>
2019-11-03 12:08:33 +0100

    add TJV PI

--- lib/analytics_event.rb
+++ lib/analytics_event.rb
@@ -3,7 +3,8 @@ module AnalyticsEvent
 
   # Custom analytics events
   module Action
-    MOZAIK_VEZA_EXIT = 'MozaikVeza.hr Exit'
+    MOZAIK_VEZA_EXIT = 'MozaikVeza.hr Exit',
+    TJV_PI_EXIT = 'tjv.PristupInfo.hr Exit'
   end
 
 end
