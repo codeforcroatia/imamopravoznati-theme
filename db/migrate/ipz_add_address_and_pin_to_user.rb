89433ba213b84ac0d3f9d4376a5e6f7617cb6b63
Miroslav <schlos@users.noreply.github.com>
2022-01-20 23:38:08 +0100

    Specifying the Rails release the migration was written for #130

--- db/migrate/ipz_add_address_and_pin_to_user.rb
+++ db/migrate/ipz_add_address_and_pin_to_user.rb
@@ -1,5 +1,5 @@
 # -*- encoding : utf-8 -*-
-class IpzAddAddressAndDobToUser < ActiveRecord::Migration
+class IpzAddAddressAndDobToUser < ActiveRecord::Migration[4.2]
   def self.up
     add_column :users, :address, :string
     add_column :users, :national_id_number, :string
