9dbadd711e9f1c623845787aae321f563363dfc5
Kruno <kruno@gmail.om>
2026-02-10 20:59:34 +0100

    Patch to last commit

--- lib/model_patches.rb
+++ lib/model_patches.rb
@@ -40,12 +40,15 @@ Rails.configuration.to_prepare do
     #UserInfoRequestSentAlert._validate_callbacks.first.filter.options[:in] << 'survey_1'
 
     InfoRequest.class_eval do
+        alias_method :_orig_calculate_date_initial_request_last_sent_at,
+               :calculate_date_initial_request_last_sent_at
+
         def calculate_date_initial_request_last_sent_at
-          date = super
+          date = _orig_calculate_date_initial_request_last_sent_at
           return nil if date.nil?
-          date.to_date
+          date
         end
-        
+
         def self.theme_short_description(state)
           {
             'correction_asked' => _('Asked for correction'),
