09ed0def71694b0ac80bae9eb4372eb601d964dc
Kruno <kruno@gmail.om>
2026-02-10 21:28:03 +0100

    Add patch for requests with Čeka odgovor status

--- lib/model_patches.rb
+++ lib/model_patches.rb
@@ -39,7 +39,13 @@ Rails.configuration.to_prepare do
     #
     #UserInfoRequestSentAlert._validate_callbacks.first.filter.options[:in] << 'survey_1'
 
-    InfoRequest.class_eval do
+    InfoRequest.class_eval do      
+        def smtp_message_ids
+          logs = mail_server_logs
+          return [] if logs.nil?
+          logs.map { |l| l['message-id'] }.compact
+        end
+        
         def calculate_date_initial_request_last_sent_at
           event = last_event_forming_initial_request
           return nil if event.nil?
