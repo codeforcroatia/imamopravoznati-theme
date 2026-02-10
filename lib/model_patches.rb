54c80fef02fb398e139fb846fbf0813e28c1ebf0
Kruno <kruno@gmail.om>
2026-02-10 19:24:34 +0100

    Fix model_patches.rb emai_subject_reqeuest - remove deprecated law_used_human() function

--- lib/model_patches.rb
+++ lib/model_patches.rb
@@ -111,11 +111,11 @@ Rails.configuration.to_prepare do
           if is_batch && public_body && public_body.url_name == 'general_register_office'
             # without GQ in the subject, you just get an auto response
             _('{{law_used_full}} request GQ - {{title}}',
-              law_used_full: law_used_human(:full),
+              law_used_full: legislation&.to_s(:full),
               title: subject_title)
           else
             _('{{law_used_full}} request - {{title}}',
-              law_used_full: law_used_human(:full),
+              law_used_full: legislation&.to_s(:full),
               title: subject_title)
           end
         end
